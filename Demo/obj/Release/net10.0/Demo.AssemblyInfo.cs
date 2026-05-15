407e2fcf41faa0ab86a89d3855c935a7f85779ef
Mudireddy <2492313@cognizant.com>
2026-05-15 12:32:14 +0530

    add reddy file intoit

--- Demo/obj/Release/net10.0/Demo.AssemblyInfo.cs
+++ Demo/obj/Release/net10.0/Demo.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45ac91666a0742662e971b0f36645217389dc874")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo")]
